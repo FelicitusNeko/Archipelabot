8a4d4cf4a0ed9b6a45f9b7d6ddbae3940cb2b234
Felix <felicitusneko@vivaldi.net>
2021-11-27 00:53:12 -0400

    basic bot client function

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
+import { Client as DiscordClient, Intents } from "discord.js";
+import * as botConf from "./botconf.json";
 
-export function sayHelloWorld(world: string) {
-  return `Hello ${world}`;
+interface BotConf {
+  discord: {
+    token: string;
+  };
 }
+
+(async () => {
+  const client = new DiscordClient({ intents: [Intents.FLAGS.GUILDS] });
+
+  client.once('ready', () => console.log('Ready'));
+
+  client.login((botConf as BotConf).discord.token);
+})();
