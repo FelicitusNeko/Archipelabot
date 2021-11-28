51ffea7db14806f520306e6d45f016d04ba25bca
Felix <felicitusneko@vivaldi.net>
2021-11-28 17:36:59 -0400

    modify intents

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,13 @@ import { Client as DiscordClient, Intents } from "discord.js";
 import { Archipelabot } from "./Archipelabot";
 
 (async () => {
-  new Archipelabot(new DiscordClient({ intents: [Intents.FLAGS.GUILDS] }));
+  new Archipelabot(
+    new DiscordClient({
+      intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+      ],
+    })
+  );
 })();
