425720b471770c24067854034ae54b5e7851dc57
Felix <felicitusneko@vivaldi.net>
2021-12-11 02:47:52 -0400

    add Direct Message intent

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ import { Archipelabot } from "./Archipelabot";
         Intents.FLAGS.GUILDS,
         Intents.FLAGS.GUILD_MESSAGES,
         Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+        Intents.FLAGS.DIRECT_MESSAGES,
       ],
     })
   );
