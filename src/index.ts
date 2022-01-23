71179f1c8c2e1732dc8eb963bfa296f5e4ad497b
Felix R <felicitusneko@vivaldi.net>
2022-01-23 04:10:38 +0000

    warn if paths are missing from env

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,25 @@
 import { Client as DiscordClient, Intents } from "discord.js";
+import { existsSync } from "fs";
+import { join as pathJoin } from "path";
 
 import { Archipelabot } from "./Archipelabot";
 
+const { PYTHON_PATH, AP_PATH } = process.env;
+
 (async () => {
+  if (!PYTHON_PATH)
+    console.warn(
+      "No Python path specified. Bot will not be able to run games."
+    );
+  if (!AP_PATH)
+    console.warn(
+      "No Archipelago path specified. Bot will not be able to run games."
+    );
+  else if (!existsSync(pathJoin(AP_PATH, "Generate.py")))
+    console.warn(
+      "Archipelago path provided seems to be missing files. Bot may not be able to run games."
+    );
+
   new Archipelabot(
     new DiscordClient({
       intents: [
